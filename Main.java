e23f2571ccbbbcc020be88a9d8f5e500393cdaab
TheRealQuentin765 <quentin@team-farrell.com>
2023-05-22 08:27:07 -0700

    Remove timer before the thing happens

--- Main.java
+++ Main.java
@@ -1,22 +1,5 @@
 public class Main {
     public static void main(String[] arg) {
-        TimerOutput output = new TimerOutput() {
-            @Override
-            public void update(long current, long length) {
-                System.out.println(100 - 100. * current / length + "%");
-            }
-
-            @Override
-            public void finish() {
-                System.out.println("BEEP BEEP BEEP");
-                Beeper.beep();
-            }
-        };
-
-
-        TimerTracker core = new TimerTracker(output,123);
-        core.countDown(1234);
-
         GUI stopGUI = new GUI();
         stopGUI.stopWatchGUI();
 
